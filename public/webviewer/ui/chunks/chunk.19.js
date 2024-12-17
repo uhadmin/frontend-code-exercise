aa6e622a0edbdb7a80e00c68461236fbe6b4f1d6
bcatoip <143360364+bcatoip@users.noreply.github.com>
2024-12-17 12:36:11 -0500

    feat: create FE coding exercise

---
+++ public/webviewer/ui/chunks/chunk.19.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{1537:function(n,r,t){"use strict";var u=t(49),f=t(38),o=t(437),c=t(281),i=o.ArrayBuffer;u({global:!0,constructor:!0,forced:f.ArrayBuffer!==i},{ArrayBuffer:i}),c("ArrayBuffer")},1538:function(n,r,t){t(568)("Int8",(function(n){return function(r,t,u){return n(this,r,t,u)}}))},1539:function(n,r,t){t(568)("Uint8",(function(n){return function(r,t,u){return n(this,r,t,u)}}),!0)}}]);
+//# sourceMappingURL=chunk.19.js.map
---
+++ src/app/document-search-controls/document-search-combobox.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { startTransition } from "react";
+import { ResultSelectionButtons } from "./result-selection-buttons";
+import {
+  Combobox,
+  ComboboxList,
+  ComboboxProvider,
+  useComboboxStore,
+} from "@ariakit/react";
+
+export type DocumentSearchComboboxProps = {
+  searching?: boolean;
+  searchValue: string;
+  selectedIndex?: number;
+  resultsCount?: number;
+  children: React.ReactNode;
+  onSearch: (searchValue: string) => void;
+  onSelectIndex: (index: number) => void;
+};
+
+export function DocumentSearchCombobox({
+  searching,
+  searchValue,
+  selectedIndex,
+  resultsCount,
+  children,
+  onSearch,
+  onSelectIndex,
+}: DocumentSearchComboboxProps) {
+  const combobox = useComboboxStore({
+    disclosure: null,
+    popover: null,
+    virtualFocus: true,
+    includesBaseElement: false,
+    value: searchValue,
+    setValue(value) {
+      startTransition(() => {
+        onSearch(value);
+      });
+    },
+    setSelectedValue(value) {
+      // multi-select not used here
+      if (Array.isArray(value)) return;
+
+      let nextSelectedIndex;
+
+      try {
+        nextSelectedIndex = parseInt(value, 10);
+      } catch {
+        console.warn("Received non-integer value:", value);
+        return;
+      }
+
+      onSelectIndex(nextSelectedIndex);
+    },
+  });
+
+  const moveSelection = (id: string | null | undefined) => {
+    const item = combobox.item(id);
+    combobox.move(id);
+    combobox.setSelectedValue(item?.value ?? "");
+  };
+
+  return (
+    <ComboboxProvider store={combobox}>
+      <div className="flex h-full flex-col">
+        <div className="flex flex-col gap-2 border-b border-gray-200 px-4 pb-2 pt-4">
+          <div className="relative h-8">
+            <Combobox
+              placeholder="Search document content"
+              className="h-8 w-full rounded-xl border-gray-300 pl-10 text-sm leading-none"
+            />
+          </div>
+          // TODO We need some buttons to navigate the list!!
+        </div>
+        <ComboboxList
+          alwaysVisible
+          className="divide-y divide-gray-200 overflow-auto"
+        >
+          {children}
+        </ComboboxList>
+      </div>
+    </ComboboxProvider>
+  );
+}
